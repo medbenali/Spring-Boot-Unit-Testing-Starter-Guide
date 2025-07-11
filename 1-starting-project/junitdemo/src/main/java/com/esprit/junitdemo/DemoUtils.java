64b2ecf69d25d6052bcb82a67e53ebf2f0d60994
medbenali <mohamed.benali@esprit.tn>
2025-07-11 10:54:59 +0100

    junit coding asserts

---
+++ 1-starting-project/junitdemo/src/main/java/com/esprit/junitdemo/DemoUtils.java
@@ -0,0 +1,26 @@
+package com.esprit.junitdemo;
+
+public class DemoUtils {
+
+    public static void main(String[] args)
+    {
+
+    }
+
+    public int add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public Object checkNull(Object obj)
+    {
+
+        if (obj != null)
+        {
+            return obj;
+        }
+        return null;
+
+
+    }
+}
